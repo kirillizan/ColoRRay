077823f166a333fe689909c46f01864321a53e82
evgeny <fire-7@ya.ru>
2016-12-19 23:56:14 +0300

    updated gtid

--- js/init.js
+++ js/init.js
@@ -9,6 +9,10 @@ function init() {
 
     saveRating();
     displayRatings();
+
+    //draw grid
+    var hexagonGrid = new HexagonGrid("HexCanvas", 50);
+    hexagonGrid.drawHexGrid(7, 10, 50, 50, true);
 }
 
 $(window).on('resize', function() {
